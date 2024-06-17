cd2b1ddf8aee6962c989bed57bae70cde42bafd5
Networksx333 <127897894+Networksx333@users.noreply.github.com>
2024-06-17 22:42:07 +0300

    17-Mon-06_2024

--- update.bat
+++ update.bat
@@ -1,7 +1,7 @@
 @echo off
 rem Получаем текущее время и дату в формате YYYY-MM-DD_HH-MM-SS
 for /f "tokens=1-4 delims=/:. " %%a in ("%date% %time%") do (
-    set datetime=%%c-%%a-%%b_%%d-%%e-%%f
+    set datetime=%%c-%%a-%%b_%%d
 )
 
 rem Выполняем команду git add
@@ -13,5 +13,5 @@ git commit -m "%datetime%"
 rem Выполняем команду git push для отправки коммита в удаленный репозиторий
 git push
 
-echo Коммит с названием %datetime% успешно создан и отправлен.
+echo Commit %datetime% created and sended.
 pause
